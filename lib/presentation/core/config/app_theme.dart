c55dba0186f17543a6e061d06f261b0a4e9b512d
Julian Aryo <julian.aryo1989@gmail.com>
2022-03-26 09:34:07 +0800

    mac

--- lib/presentation/core/config/app_theme.dart
+++ lib/presentation/core/config/app_theme.dart
@@ -4,29 +4,31 @@ import 'package:moslem/presentation/core/model/theme_model.dart';
 class AppTheme {
   static final ThemeModel light = ThemeModel(
     true,
-    primaryColor: const Color(0xff16C172),
-    onPrimaryColor: const Color(0xffFBFFFE),
-    secondaryColor: const Color(0xff3AAED8),
-    onSecondaryColor: const Color(0xffFBFFFE),
-    variantColor: const Color(0xff12263A),
-    onVariantColor: const Color(0xffFBFFFE),
-    accentColor: const Color(0xffF6EAA2),
-    onAccentColor: const Color(0xff272932),
-    backgroundColor: const Color(0xffFBFFFE),
-    onBackgroundColor: const Color(0xff272932),
+    primaryColor: const Color(0xff1687A7),
+    onPrimaryColor: const Color(0xffF6F5F5),
+    accentColor: const Color(0xffD3E0EA),
+    onAccentColor: const Color(0xff276678),
+    backgroundColor: const Color(0xffF6F5F5),
+    onBackgroundColor: const Color(0xff276678),
   );
 
   static final ThemeModel dark = ThemeModel(
     false,
-    primaryColor: const Color(0xff5986FF),
-    onPrimaryColor: const Color(0xffFBFFFE),
-    secondaryColor: const Color(0xff3A8DD8),
-    onSecondaryColor: const Color(0xffFBFFFE),
-    variantColor: const Color(0xff12203A),
-    onVariantColor: const Color(0xffFBFFFE),
-    accentColor: const Color(0xffDED5BA),
-    onAccentColor: const Color(0xff272932),
-    backgroundColor: const Color(0xff272932),
-    onBackgroundColor: const Color(0xffFBFFFE),
+    primaryColor: const Color(0xff00ADB5),
+    onPrimaryColor: const Color(0xffEEEEEE),
+    accentColor: const Color(0xff393E46),
+    onAccentColor: const Color(0xffEEEEEE),
+    backgroundColor: const Color(0xff222831),
+    onBackgroundColor: const Color(0xffEEEEEE),
   );
 }
+
+// static final ThemeModel light = ThemeModel(
+//     true,
+//     primaryColor: const Color(0xff3F72AF),
+//     onPrimaryColor: const Color(0xffF9F7F7),
+//     accentColor: const Color(0xffDBE2EF),
+//     onAccentColor: const Color(0xff112D4E),
+//     backgroundColor: const Color(0xffF9F7F7),
+//     onBackgroundColor: const Color(0xff112D4E),
+//   );
--- lib/presentation/page/intro/intro_page.dart
+++ lib/presentation/page/intro/intro_page.dart
@@ -1,3 +1,4 @@
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:moslem/presentation/core/config/app_theme.dart';
@@ -12,58 +13,345 @@ class IntroPage extends GetView<IntroPageController> {
   @override
   Widget build(BuildContext context) {
     return Obx(() {
-      final theme = globalService.theme;
+      final theme = globalService.theme();
       return Scaffold(
-        backgroundColor: theme().backgroundColor,
+        backgroundColor: theme.backgroundColor,
         body: SafeArea(
-          child: Column(children: [
-            Padding(
-              padding: EdgeInsets.all(theme().mediumPadding),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  CircleAvatar(
-                    radius: 18,
-                    backgroundColor:
-                        theme().isLight ? Colors.grey[100] : Colors.grey[800],
-                    child: IconButton(
-                      onPressed: () {
-                        globalService.theme.value =
-                            (theme().isLight) ? AppTheme.dark : AppTheme.light;
-                      },
-                      constraints: const BoxConstraints(),
-                      padding: const EdgeInsets.all(0),
-                      icon: Icon(
-                        theme().isLight ? Icons.dark_mode : Icons.sunny,
-                        color: theme().isLight ? Colors.black45 : Colors.amber,
-                      ),
-                    ),
-                  ),
-                  Column(
-                    crossAxisAlignment: CrossAxisAlignment.end,
-                    children: [
-                      Text(
-                        '13 Shaban, 1442 AH',
-                        style: TextStyle(
-                          fontWeight: FontWeight.w600,
-                          fontSize: theme().bigFont,
-                          color: theme().onBackgroundColor,
+          child: SingleChildScrollView(
+            child: Column(children: [
+              Padding(
+                padding: EdgeInsets.all(theme.mediumPadding),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    CircleAvatar(
+                      radius: 18,
+                      backgroundColor:
+                          theme.isLight ? Colors.grey[300] : Colors.black12,
+                      child: IconButton(
+                        onPressed: () {
+                          globalService.theme.value =
+                              (theme.isLight) ? AppTheme.dark : AppTheme.light;
+                        },
+                        constraints: const BoxConstraints(),
+                        padding: const EdgeInsets.all(0),
+                        icon: Icon(
+                          theme.isLight
+                              ? Icons.dark_mode_rounded
+                              : Icons.wb_sunny_rounded,
+                          color: theme.isLight
+                              ? Colors.black38
+                              : Colors.amber[400],
                         ),
                       ),
-                      Text(
-                        'Jummat, 25 Maret',
-                        style: TextStyle(
-                          fontSize: theme().smallFont,
-                          color: theme().onBackgroundColor,
+                    ),
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.end,
+                      children: [
+                        Text(
+                          '13 Shaban, 1442 AH',
+                          style: TextStyle(
+                            fontWeight: FontWeight.w600,
+                            fontSize: theme.bigFont,
+                            color: theme.onBackgroundColor,
+                          ),
+                        ),
+                        Text(
+                          'Jummat, 25 Maret',
+                          style: TextStyle(
+                            fontSize: theme.smallFont,
+                            color: theme.onBackgroundColor,
+                          ),
+                        ),
+                      ],
+                    )
+                  ],
+                ),
+              ),
+              SizedBox(height: theme.mediumPadding),
+              Padding(
+                padding: EdgeInsets.symmetric(horizontal: theme.mediumPadding),
+                child: Ink(
+                  height: Get.size.width / 2 - theme.mediumPadding,
+                  decoration: BoxDecoration(
+                    color: theme.accentColor,
+                    borderRadius: BorderRadius.circular(20),
+                  ),
+                  child: Stack(
+                    children: [
+                      Align(
+                        alignment: Alignment.bottomRight,
+                        child: Padding(
+                          padding: const EdgeInsets.only(right: 6, bottom: 2),
+                          child: CachedNetworkImage(
+                            imageUrl:
+                                'https://cdn-icons-png.flaticon.com/512/1146/1146869.png',
+                            width: 100,
+                          ),
+                        ),
+                      ),
+                      Padding(
+                        padding: EdgeInsets.all(theme.mediumPadding),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              'Fajar',
+                              style: TextStyle(
+                                color: theme.onAccentColor,
+                                fontWeight: FontWeight.w600,
+                                fontSize: 20,
+                              ),
+                            ),
+                            Text(
+                              '04:58 AM',
+                              style: TextStyle(
+                                color: theme.primaryColor,
+                                fontWeight: FontWeight.w600,
+                                fontSize: 40,
+                              ),
+                            ),
+                            Text(
+                              '13 menit sebelum azan',
+                              style: TextStyle(
+                                color: theme.onAccentColor,
+                                fontSize: 14,
+                              ),
+                            ),
+                          ],
                         ),
                       ),
                     ],
-                  )
-                ],
+                  ),
+                ),
               ),
-            ),
-          ]),
+              SizedBox(height: theme.mediumPadding),
+              Padding(
+                padding: EdgeInsets.symmetric(horizontal: theme.mediumPadding),
+                child: Row(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Flexible(
+                      flex: 1,
+                      fit: FlexFit.tight,
+                      child: Ink(
+                        height: Get.size.width / 2 - theme.mediumPadding,
+                        decoration: BoxDecoration(
+                          color: theme.accentColor,
+                          borderRadius: BorderRadius.circular(20),
+                        ),
+                        child: Stack(
+                          children: [
+                            Align(
+                              alignment: Alignment.bottomRight,
+                              child: Padding(
+                                padding:
+                                    const EdgeInsets.only(right: 6, bottom: 2),
+                                child: CachedNetworkImage(
+                                  imageUrl:
+                                      'https://cdn-icons-png.flaticon.com/512/869/869869.png',
+                                  width: 80,
+                                ),
+                              ),
+                            ),
+                            Padding(
+                              padding: EdgeInsets.all(theme.mediumPadding),
+                              child: Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  Text(
+                                    'Dhuhr',
+                                    style: TextStyle(
+                                      color: theme.onAccentColor,
+                                      fontWeight: FontWeight.w600,
+                                      fontSize: 16,
+                                    ),
+                                  ),
+                                  Text(
+                                    '12:17 PM',
+                                    style: TextStyle(
+                                      color: theme.primaryColor,
+                                      fontWeight: FontWeight.w600,
+                                      fontSize: 20,
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                    SizedBox(width: theme.mediumPadding),
+                    Flexible(
+                      flex: 1,
+                      fit: FlexFit.tight,
+                      child: Ink(
+                        height: Get.size.width / 2 - theme.mediumPadding,
+                        decoration: BoxDecoration(
+                          color: theme.accentColor,
+                          borderRadius: BorderRadius.circular(20),
+                        ),
+                        child: Stack(
+                          children: [
+                            Align(
+                              alignment: Alignment.bottomRight,
+                              child: Padding(
+                                padding:
+                                    const EdgeInsets.only(right: 6, bottom: 2),
+                                child: CachedNetworkImage(
+                                  imageUrl:
+                                      'https://cdn-icons-png.flaticon.com/512/414/414927.png',
+                                  width: 80,
+                                ),
+                              ),
+                            ),
+                            Padding(
+                              padding: EdgeInsets.all(theme.mediumPadding),
+                              child: Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  Text(
+                                    'Ashar',
+                                    style: TextStyle(
+                                      color: theme.onAccentColor,
+                                      fontWeight: FontWeight.w600,
+                                      fontSize: 16,
+                                    ),
+                                  ),
+                                  Text(
+                                    '03:22 PM',
+                                    style: TextStyle(
+                                      color: theme.primaryColor,
+                                      fontWeight: FontWeight.w600,
+                                      fontSize: 20,
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              SizedBox(height: theme.mediumPadding),
+              Padding(
+                padding: EdgeInsets.symmetric(horizontal: theme.mediumPadding),
+                child: Row(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Flexible(
+                      flex: 1,
+                      fit: FlexFit.tight,
+                      child: Ink(
+                        height: Get.size.width / 2 - theme.mediumPadding,
+                        decoration: BoxDecoration(
+                          color: theme.accentColor,
+                          borderRadius: BorderRadius.circular(20),
+                        ),
+                        child: Stack(
+                          children: [
+                            Align(
+                              alignment: Alignment.bottomRight,
+                              child: Padding(
+                                padding:
+                                    const EdgeInsets.only(right: 6, bottom: 4),
+                                child: CachedNetworkImage(
+                                  imageUrl:
+                                      'https://cdn-icons-png.flaticon.com/512/3094/3094159.png',
+                                  width: 70,
+                                ),
+                              ),
+                            ),
+                            Padding(
+                              padding: EdgeInsets.all(theme.mediumPadding),
+                              child: Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  Text(
+                                    'Maghrib',
+                                    style: TextStyle(
+                                      color: theme.onAccentColor,
+                                      fontWeight: FontWeight.w600,
+                                      fontSize: 16,
+                                    ),
+                                  ),
+                                  Text(
+                                    '06:21 PM',
+                                    style: TextStyle(
+                                      color: theme.primaryColor,
+                                      fontWeight: FontWeight.w600,
+                                      fontSize: 20,
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                    SizedBox(width: theme.mediumPadding),
+                    Flexible(
+                      flex: 1,
+                      fit: FlexFit.tight,
+                      child: Ink(
+                        height: Get.size.width / 2 - theme.mediumPadding,
+                        decoration: BoxDecoration(
+                          color: theme.accentColor,
+                          borderRadius: BorderRadius.circular(20),
+                        ),
+                        child: Stack(
+                          children: [
+                            Align(
+                              alignment: Alignment.bottomRight,
+                              child: Padding(
+                                padding:
+                                    const EdgeInsets.only(right: 6, bottom: 2),
+                                child: CachedNetworkImage(
+                                  imageUrl:
+                                      'https://cdn-icons.flaticon.com/png/512/2387/premium/2387889.png?token=exp=1648231029~hmac=ed7889c8c636e4b5133f77b0b7989f78',
+                                  width: 80,
+                                ),
+                              ),
+                            ),
+                            Padding(
+                              padding: EdgeInsets.all(theme.mediumPadding),
+                              child: Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  Text(
+                                    'Isha',
+                                    style: TextStyle(
+                                      color: theme.onAccentColor,
+                                      fontWeight: FontWeight.w600,
+                                      fontSize: 16,
+                                    ),
+                                  ),
+                                  Text(
+                                    '07:29 PM',
+                                    style: TextStyle(
+                                      color: theme.primaryColor,
+                                      fontWeight: FontWeight.w600,
+                                      fontSize: 20,
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ]),
+          ),
         ),
       );
     });
